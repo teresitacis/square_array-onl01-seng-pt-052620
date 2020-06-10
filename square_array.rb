f37caceec697c97ffb45c7ec4136647f79a7d14c
teresita cisneros estrada <cisneros0831@gmail.com>
2020-06-10 04:40:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+
+def square_array (array)
+  new_array = []
+  array.each { |a| new_array,push (a*a)}
+  return new_array
 end
+
+arr = [1, 2 ,3, 4, 5]
+square_array(arr)
